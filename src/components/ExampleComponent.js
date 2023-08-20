24431c26218c160d57683a8cb6c3956d18648968
Eman Gurung <egurung1117@gmail.com>
2023-08-19 23:25:12 -0400

    complete

--- src/components/ExampleComponent.js
+++ src/components/ExampleComponent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import TestComponent from "./TestComponent";
 
 // no need to modify anything in here. The second test is looking for the returned JSX from this component
 function ExampleComponent() {
@@ -9,6 +10,7 @@ function ExampleComponent() {
         alt="a deeply impressed man, perhaps thinking 'whoa'"
       />
       <p>Whoa!</p>
+      <TestComponent/>
     </div>
   );
 }
